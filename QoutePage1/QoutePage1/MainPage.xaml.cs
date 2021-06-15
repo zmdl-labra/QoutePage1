c1d7e07633d40fce0ec0ab3173b44698a0ececfc
IVY-RECTA <IVY-RECTA@DESKTOP-4C0VJMV>
2021-06-15 16:23:17 +0800

    Add project files.

---
+++ QoutePage1/QoutePage1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QoutesPage
+{
+    public partial class MainPage : ContentPage
+    {
+        private int _index = 0;
+        private string[] _quotes = new string[]
+        {
+            "mbbjbjvjvjvjbnb",
+            "xxxxxxxxxxxx",
+            "cccccccccccccccc"
+        };
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            currentQuote.Text = _quotes[_index];
+
+
+
+
+        }
+
+        private void NEXT_Clicked(object sender, EventArgs e)
+        {
+
+            _index++;
+            if (_index >= _quotes.Length)
+                _index = 0;
+            currentQuote.Text = _quotes[_index];
+        }
+    }
+}
